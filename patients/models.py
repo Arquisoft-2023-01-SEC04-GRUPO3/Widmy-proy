97b73e50d46f6f4a1a5a04c814a593bc45b2df74
leo283 <gl.moreno@uniandes.edu.co>
2023-03-17 22:22:44 -0500

    update

--- patients/models.py
+++ patients/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Patient(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField(default=0)
-    blood_type = models.CharField(max_length=3)
+    blood_type = models.CharField(max_length=3,default='N/A')
     weight = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
 
