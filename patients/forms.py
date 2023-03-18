3142697b40a180b966c23ebc56557de5ae36547e
leo283 <gl.moreno@uniandes.edu.co>
2023-03-17 21:58:01 -0500

    hello

--- patients/forms.py
+++ patients/forms.py
@@ -5,16 +5,8 @@ class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
         fields = [
-            'name',
-            'age',
-            'blood_type',
-            'weight',
-            'height'
+            'name'
         ]
         labels = {
-            'name': 'Name',
-            'age': 'Age',
-            'blood_type': 'Blood Type',
-            'weight': 'Weight',
-            'height': 'Height'
+            'name': 'Name'
         }
