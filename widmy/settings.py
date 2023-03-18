8367a59e2fbff745ce9c3eab2011bf5524c801be
leo283 <gl.moreno@uniandes.edu.co>
2023-03-17 21:27:55 -0500

    questapasando

--- widmy/settings.py
+++ widmy/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'widmy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR/ 'widmy', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'widmy', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
