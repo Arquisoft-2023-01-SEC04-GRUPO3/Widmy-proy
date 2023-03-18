90583a789a086afacb551a4bf5c82019b49b06fd
leo283 <gl.moreno@uniandes.edu.co>
2023-03-17 21:59:19 -0500

    hello

--- patients/models.py
+++ patients/models.py
@@ -2,10 +2,7 @@ from django.db import models
 
 class Patient(models.Model):
     name = models.CharField(max_length=50)
-    age = models.IntegerField()
-    blood_type = models.CharField(max_length=3)
-    weight = models.FloatField()
-    height = models.FloatField()
+
 
     def __str__(self):
         return '{}'.format(self.name)
